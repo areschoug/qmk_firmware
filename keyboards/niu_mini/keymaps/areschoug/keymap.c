eb435bfe21e08b8d9fa610600c0bc6bd76da3823
Andreas Areschoug <andreas.areschoug@gmail.com>
2021-01-27 17:34:04 +0100

    Rev 3

--- keyboards/niu_mini/keymaps/areschoug/keymap.c
+++ keyboards/niu_mini/keymaps/areschoug/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,             KC_Q,    KC_W,     KC_E,    KC_R,                KC_T, KC_Y,    KC_U,               KC_I,    KC_O,    KC_P,    KC_AA,
         LT(_RAISE, KC_TAB), KC_A,    KC_S,     KC_D,    KC_F,                KC_G, KC_H,    KC_J,               KC_K,    KC_L,    KC_OE,   KC_AE,
         KC_LSFT,            KC_Z,    KC_X,     KC_C,    KC_V,                KC_B, KC_N,    KC_M,               KC_COMM, KC_DOT,  SE_MINS,  RSFT_T(KC_ENT),
-        KC_LCTL,            KC_LCMD, MO(_NUM), KC_LALT, LT(_LOWER, KC_BSPC), CMD_T(KC_SPC), LT(_LOWER, KC_ENT), KC_SPC,  MO(_NUM), MO(_ADJ), MO(_FN)
+        KC_LCTL,            KC_LCMD, MO(_NUM), KC_LALT, LT(_LOWER, KC_BSPC), CMD_T(KC_SPC), LT(_NUM, KC_ENT), KC_SPC,  MO(_NUM), MO(_ADJ), MO(_FN)
     ),
 
     /* LOWER
